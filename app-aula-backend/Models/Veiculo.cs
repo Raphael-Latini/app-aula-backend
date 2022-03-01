2ca076911740275f8b6fc3458b449caa6d9abb33
Raphael Latini <raphael.latini@gmail.com>
2022-02-28 21:11:06 -0300

    Adicionar arquivos de projeto.

---
+++ app-aula-backend/Models/Veiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_aula_backend.Models
+{   [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="obrigatorio informar o nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage ="obrigatorio informar a placa!")]
+        public string Placa { get; set; }
+
+
+    }
+}
